da91e3e218aabb1b06b12113860ddc0683d206fa
em5728jo-s <ta7641it-s@student.lu.se>
2018-09-27 16:01:34 +0200

    added ex5 files

---
+++ ex5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	int A, B;
+	std::cout << "Enter first number";
+	std::cin >> A;
+	std::cout << "Enter second number";
+	std::cin >> B;
+	if (A<B){
+		std::cout << B;}
+	
+	else std::cout << A;
+
+}
