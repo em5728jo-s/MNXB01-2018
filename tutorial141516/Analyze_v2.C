436b8109f0bbe0aaec778bb8826d480f518274cc
em5728jo-s <em5728jo-s@student.lu.se>
2018-10-26 14:04:32 +0200

    changed things

--- tutorial141516/Analyze_v2.C
+++ tutorial141516/Analyze_v2.C
@@ -14,17 +14,18 @@ void analyze_v2_2particle(){
 	
 	Int_t nEvents = 0; // event counter
 	string helpString; // help variable
+	Double_t eventavg2A = 0;
+	Double_t eventavg2QA = 0;
 	
 	// open input file
 	ifstream file("phi_dist.dat");
 	Int_t eventNo = -1;
-	Int_t nTracks = -1;
-	Double_t sum_cos2_diff = 0;
 
 	while(file >> helpString >> eventNo) {
-		cout << "Reading event : " << eventNo << endl;
+		cout << "\nReading event : " << eventNo << endl;
 		nEvents++;
 		
+		Int_t nTracks = -1;
 		file >> helpString >> nTracks;
 		cout << "Event contains " << nTracks << " tracks" << endl;
 		
@@ -38,22 +39,58 @@ void analyze_v2_2particle(){
 		}
 		// Here we want to analyze the phi values in the next exercise
 		
-		for(Int_t i = 0; i < nTracks; i++) {
-			for(Int_t j = i+1; j < nTracks; j++) {
+		//Q vector
+		Double_t sum_cos2 = 0;
+		Double_t sum_sin2 = 0;
+		
+		//2-particle
+		Double_t sum_cos2_diff = 0;
+		for (Int_t i = 0; i < nTracks; i++) {
+			
+			//Compute Q vector
+			sum_cos2 += TMath::Cos(2*phi[i]);
+			sum_sin2 += TMath::Sin(2*phi[i]);
+			
+			//Compute 2-particle
+			for (Int_t j = i+1; j < nTracks; j++) {
 				
 				sum_cos2_diff += 2*TMath::Cos(2*(phi[i]-phi[j]));
 			}
 		}
-	}
-	
-	file.close();
 		
 		// calculate <2> for this event
 		Double_t eventavg2 = (1./(nTracks*(nTracks-1)))*sum_cos2_diff;
+		Double_t eventavg2Q = (pow(sum_cos2,2)+pow(sum_sin2,2)-nTracks)/(nTracks*(nTracks-1));
 		cout << "<2> = " << eventavg2 << endl;
 		cout << "sum_cos2_diff = " << sum_cos2_diff << endl;
+		cout << "Q: <2> = " << eventavg2Q << endl;
+		if (eventavg2 < 0) {
+			cout << "<2> negative, no v2." << endl;
+		}
+		else {
+			cout << "v2 (for this event) = " << sqrt(eventavg2) << endl;
+		}
 		
-		// Update <2> event average for Q-vector and 2-particle
-		//....
+		if (eventavg2Q < 0){
+			cout << "Q: <2> negative, no v2." << endl;
+		}
+		
+		else {
+			cout << "Q: v2 (for this event) = " << sqrt(eventavg2Q) << endl;
+		}
+		
+		if (eventavg2 > 0) {
+			eventavg2A += sqrt(eventavg2);
+		}
+		if (eventavg2Q > 0) {
+			eventavg2QA += sqrt(eventavg2Q);
+		}
+		
+	}
+
+	cout << "v2 (average of 10 events) = " << eventavg2A/nEvents << endl;
+	cout << "Q: v2 (average of 10 events) = " << eventavg2QA/nEvents << endl;
+	
+	file.close();
 
 }
